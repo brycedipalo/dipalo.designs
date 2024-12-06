5e8b0cfe125d83caf5f3ace328ac720450af8e5b
Bryce <bdipalo@hotmail.com>
2024-12-06 09:02:27 -0800

    fixed tailwind

---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    './index.html',
+  ], // or adjust if your HTML/JS files are located elsewhere
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
